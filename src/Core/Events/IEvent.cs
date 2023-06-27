3664e6788fbd33800dc15b88b695a972f0997c56
Thismaker <smithkare47@gmail.com>
2023-06-27 17:54:33 +0300

    update: introduce ITimedEvent

--- src/Core/Events/IEvent.cs
+++ src/Core/Events/IEvent.cs
@@ -1,15 +1,11 @@
-﻿using System;
-
-namespace Ocluse.LiquidSnow.Core.Events
+﻿namespace Ocluse.LiquidSnow.Core.Events
 {
+
     /// <summary>
     /// An action or occurrence in the system that can be reacted to.
     /// </summary>
     public interface IEvent
     {
-        /// <summary>
-        /// The time when the event occurred.
-        /// </summary>
-        DateTime Timestamp { get; }
+
     }
 }
